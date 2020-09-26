a718ce854ab9290a4f42d50b6cc72020ff8fdd4b
tinolebat <jsdah07@gmail.com>
2020-09-27 01:22:25 +0800

    Add function to load config file + check authorizationfor client

--- src/server/go.mod
+++ src/server/go.mod
@@ -2,4 +2,8 @@ module github.com/tinolebat/go-jwt/server
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.6.3
+	github.com/joho/godotenv v1.3.0
+)
--- src/server/server.go
+++ src/server/server.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+// GetEnvVariable Get the Environment variables
+func getEnvVariable(key string) string {
+	err := godotenv.Load("../config/.env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
 
 func homepage(c *gin.Context) {
 	fmt.Println("Welcome to homepage")
@@ -13,16 +25,44 @@ func homepage(c *gin.Context) {
 	})
 }
 
-func main() {
+func isAuthorized(endpoint func(c *gin.Context)) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		key := getEnvVariable("JWT_TKN_0")
+		mySigningKey := []byte(key)
+		if c.GetHeader("Token") != "" {
+
+			token, err := jwt.Parse(c.GetHeader("Token"), func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There is an error")
+				}
+				return mySigningKey, nil
+			})
+
+			if err != nil {
+				fmt.Println("Token parsing error")
+			}
+
+			if token.Valid {
+				endpoint(c)
+			}
+
+		} else {
+			fmt.Println("Not Authorized")
+			w := "Not Authorized"
+			c.JSON(200, w)
+		}
+	}
+
+}
+
+func handleRequest() {
 	router := gin.Default()
-
-	router.GET("/", func(c *gin.Context) {
-		fmt.Println("Welcome to homepage")
-		c.JSON(200, gin.H{
-			"message": "Welcome",
-		})
-	})
-
-	fmt.Println("Hi")
+	router.GET("/", isAuthorized(homepage))
 	router.Run()
+}
+
+func main() {
+	fmt.Println("Server side")
+	handleRequest()
+
 }
